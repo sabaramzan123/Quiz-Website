9aa29bca31349d6186aa28e6d36e9efd58ed8d7e
Saba Ramzan <sabaramzan0226@gmail.com>
2025-01-30 18:26:36 +0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,109 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "C. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Type Multi Language",
+      "B. Hyper Text Multiple Language",
+      "C. Hyper Text Markup Language",
+      "D. Home Text Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "A. Cascading Style Sheet",
+    options: [
+      "A. Cascading Style Sheet",
+      "B. Cute Style Sheet",
+      "C. Computer Style Sheet",
+      "D. Code Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What is the purpose of the alt attribute in an img tag?",
+    answer:
+      "B. To provide a text description of the image for accessibility purposes",
+    options: [
+      "A. To specify the width of the image",
+      "B. To provide a text description of the image for accessibility purposes",
+      "C. To link to another webpage",
+      "D. To add a tooltip to the image",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What is the purpose of the var keyword?",
+    answer: "B. Declare a variable",
+    options: [
+      "A. Declare a constant",
+      "B. Declare a variable",
+      "C. Declare a function",
+      "D. Declare an array",
+    ],
+  },
+
+  {
+    numb: 5,
+    question:
+      "What is the purpose of the addEventListener() method in JavaScript?",
+    answer:
+      "A. To attach a function to an event, such as a click or hover event",
+    options: [
+      "A. To attach a function to an event, such as a click or hover event",
+      "B. To remove an event listener from an element",
+      "C. To trigger an event programmatically",
+      "D. To prevent an event from occurring",
+    ],
+  },
+
+  {
+    numb: 6,
+    question: "Which language is used for styling web pages?",
+    answer: "C. CSS",
+    options: ["A. HTML", "B. JavaScript", "C. CSS", "D. XML"],
+  },
+
+  {
+    numb: 7,
+    question:
+      'Which is the correct JavaScript syntax to print "Hello World" in the console?',
+    answer: 'A. console.log("Hello World");',
+    options: [
+      'A. console.log("Hello World");',
+      'B. print("Hello World");',
+      'C. echo("Hello World");',
+      'D. document.write("Hello World");',
+    ],
+  },
+
+  {
+    numb: 8,
+    question:
+      "Which programming language is primarily used for web development?",
+    answer: "B. JavaScript",
+    options: ["A. Python", "B. JavaScript", "C. C++", "D. Swift"],
+  },
+
+  {
+    numb: 9,
+    question:
+      "Which method can be used to round a number to the nearest integer in JavaScript?",
+    answer: "A. Math.round()",
+    options: [
+      "A. Math.round()",
+      "B. Math.ceil()",
+      "C. Math.floor()",
+      "D. Math.abs()",
+    ],
+  },
+
+  {
+    numb: 10,
+    question: "Which of the following is a JavaScript framework?",
+    answer: "C. React",
+    options: ["A. Django", "B. Flask", "C. React", "D. Laravel]"],
+  },
+];
